a86c0fece343b7c8707b3b7afca168c63145cf50
Mark Fu <MarkFull@users.noreply.github.com>
2016-10-12 15:39:01 +0800

    Fix delete! override
    As `include` won't override the original method, reopen the class and `alias_method` did the job. Thanks to help from @ktaragorn

--- lib/couchrest/model/support/couchrest_database.rb
+++ lib/couchrest/model/support/couchrest_database.rb
@@ -3,24 +3,12 @@
 # also emptied. Given that this is a rare event, and the consequences are not 
 # very severe, we just completely empty the cache.
 #
-module CouchRest::Model
-  module Support
-    module Database
-
-      def delete!
-        Thread.current[:couchrest_design_cache] = { }
-        super
-      end
-
-      def recreate!
-        Thread.current[:couchrest_design_cache] = { }
-        super
-      end
-
-    end
-  end
-end
-
 class CouchRest::Database
-  include(CouchRest::Model::Support::Database)
+
+  alias_method :orig_delete!, :delete!
+  def delete!
+    Thread.current[:couchrest_design_cache] = { }
+    orig_delete!
+  end
+
 end
